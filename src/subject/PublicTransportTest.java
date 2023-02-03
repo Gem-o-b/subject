3c6c276401692e2228f588ee18634ea5fb1c8c02
JaeHun <clickyour@gmail.com>
2023-02-03 14:43:56 +0900

    버스,택시 클래스생성

--- src/subject/PublicTransportTest.java
+++ src/subject/PublicTransportTest.java
@@ -2,22 +2,19 @@ package subject;
 
 public class PublicTransportTest {
     public static void main(String[] args) {
-//        Bus bus1 = new Bus();
-//        Bus bus2 = new Bus();
-//        bus1.takeCar(2);
-//        bus1.setOil(-50);
-//        bus1.setState();
-//        bus1.setOil(10);
-//        bus1.setState();
-//        bus1.takeCar(45);
-//        bus1.takeCar(5);
-//        bus1.setOil(-55);
-
-
-
-
-
-
+        Bus bus1 = new Bus();
+        Bus bus2 = new Bus();
+        bus1.takeCar(2);
+        bus1.setOil(-50);
+        bus1.setState();
+        bus1.setOil(10);
+        bus1.setState();
+        bus1.takeCar(45);
+        bus1.takeCar(5);
+        bus1.setOil(-55);
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
         Texi texi1 = new Texi();
         Texi texi2 = new Texi();
